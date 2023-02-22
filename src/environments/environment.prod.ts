aaf4ee72f7a2728ae455355ee2cc318170bd7cfb
yoshihama <yuya.soccer0224@gmail.com>
2023-02-23 00:49:26 +0900

    元に戻す

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,5 @@
-import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config();
-
 export const environment = {
   production: true,
   staging: false,
-  // apiUrl: 'https://laravel-demo-prd.onrender.com/api',
-  apiUrl: process.env.API_URL,
+  apiUrl: 'https://laravel-demo-prd.onrender.com/api',
 };
--- src/environments/environment.stg.ts
+++ src/environments/environment.stg.ts
@@ -1,9 +1,5 @@
-import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config();
-
 export const environment = {
   production: false,
   staging: true,
-  // apiUrl: 'https://laravel-demo-stg.onrender.com/api',
-  apiUrl: process.env.API_URL,
+  apiUrl: 'https://laravel-demo-stg.onrender.com/api',
 };
