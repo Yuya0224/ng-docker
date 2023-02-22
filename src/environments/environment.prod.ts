3bca7f88ae48544a0f07f0c3619311e45010de60
yoshihama <yuya.soccer0224@gmail.com>
2023-02-23 00:43:28 +0900

    dotenv install

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,9 @@
+import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
+
 export const environment = {
   production: true,
   staging: false,
-  apiUrl: 'https://laravel-demo-prd.onrender.com/api',
+  // apiUrl: 'https://laravel-demo-prd.onrender.com/api',
+  apiUrl: process.env.API_URL,
 };
--- src/environments/environment.stg.ts
+++ src/environments/environment.stg.ts
@@ -1,5 +1,9 @@
+import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
+
 export const environment = {
   production: false,
   staging: true,
-  apiUrl: 'https://laravel-demo-stg.onrender.com/api',
+  // apiUrl: 'https://laravel-demo-stg.onrender.com/api',
+  apiUrl: process.env.API_URL,
 };
